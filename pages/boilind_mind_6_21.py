cb3455c9cc4f865b9de3c7e73a2fba1b1d269753
crum7 <rikugamesion@gmail.com>
2023-06-22 02:20:15 +0900

    6_21

--- pages/boilind_mind_6_21.py
+++ pages/boilind_mind_6_21.py
@@ -5,36 +5,7 @@ import glob
 import plotly.graph_objects as go
 
 
-st.title('ボーリングマインド 6/21 プロット')
-'''
-目的：ダンサー内で心拍同期が起きているかどうか\n
-詳細:ボーリングマインドのリハーサル時にダンサーの左腕にpolar verity senseをつけてもらい、パフォーマンスをしていただきました。\n
-その時の心拍(HR)をプロットしました。 また、心拍同期は心拍の増減のタイミングが同期しているかがなので、個人ごとに心拍を正規化（最小値を0最大値を1とする）し、プロットしました。\n
-'''
-st.caption('下のグラフ内の右のファイル名をクリックすると、ファイルごとに表示・非表示が切り替えられます！\nまた、グラフ右上の拡張ボタンを押すと、グラフが大きく表示されます！')
-option = st.selectbox(
-'表示するカテゴリ',
-('all','audience','dancer')
-)
-st.write(option=='all')
-
-'''
-if option == 'all':
-    files_path = './boiling_mind_6_21_data/**/*.CSV'
-    fig = plot_hr_data(glob_files_path=files_path)
-    st.plotly_chart(fig)
-elif option == 'audience':
-    files_path = './boiling_mind_6_21_data/audience/*.CSV'
-    fig = plot_hr_data(glob_files_path=files_path)
-    st.plotly_chart(fig)
-elif option == 'dancer':
-    files_path = './boiling_mind_6_21_data/dancer/*.CSV'
-    fig = plot_hr_data(glob_files_path=files_path)
-    st.plotly_chart(fig)
-'''
-'''
-
-def plot_hr_data(glob_files_path:str):
+def plot_hr(glob_files_path:str):
     files = glob.glob(glob_files_path)
     fig = go.Figure()
 
@@ -76,4 +47,32 @@ def plot_hr_data(glob_files_path:str):
     )
 
     return fig
+
+
+st.title('ボーリングマインド 6/21 プロット')
 '''
+目的：ダンサー内で心拍同期が起きているかどうか\n
+詳細:ボーリングマインドのリハーサル時にダンサーの左腕にpolar verity senseをつけてもらい、パフォーマンスをしていただきました。\n
+その時の心拍(HR)をプロットしました。 また、心拍同期は心拍の増減のタイミングが同期しているかがなので、個人ごとに心拍を正規化（最小値を0最大値を1とする）し、プロットしました。\n
+'''
+st.caption('下のグラフ内の右のファイル名をクリックすると、ファイルごとに表示・非表示が切り替えられます！\nまた、グラフ右上の拡張ボタンを押すと、グラフが大きく表示されます！')
+option = st.selectbox(
+'表示するカテゴリ',
+('all','audience','dancer')
+)
+
+
+files_path = ''
+if option == 'all':
+    files_path = './boiling_mind_6_21_data/**/*.CSV'
+    fig = plot_hr(glob_files_path=files_path)
+    st.plotly_chart(fig)
+elif option == 'audience':
+    files_path = './boiling_mind_6_21_data/audience/*.CSV'
+    fig = plot_hr(glob_files_path=files_path)
+    st.plotly_chart(fig)
+elif option == 'dancer':
+    files_path = './boiling_mind_6_21_data/dancer/*.CSV'
+    fig = plot_hr(glob_files_path=files_path)
+    st.plotly_chart(fig)
+
